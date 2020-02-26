71526df85791fe20e61ce61db630aa88694dd03f
WilyLynx <mozdzonekm@student.mini.pw.edu.pl>
2020-02-26 22:54:55 +0100

    Solved proton

--- proton/MozdzonekMichal/script.R
+++ proton/MozdzonekMichal/script.R
@@ -1,13 +1,37 @@
-john = employees[which(employees$name=='John' & employees$surname == 'Insecure'),]
+# proton game solution by Michal Mozdzonek
 
+library(BetaBit)
+library(dplyr)
+# get John from employees
+john <-  employees[which(employees$name=='John' & employees$surname == 'Insecure'),]
+
+# login as John
 proton(action= 'login', login=john$login)
 
-success <- "Success! User is logged in!"
-for (pass in top1000passwords) {
-  status = proton(action='login', login=john$login, password=pass)
-  if(status == success)
-    {
-    john_pass <-  pass
-    break
-  }
-}
+# brutforce John password
+for (pass in top1000passwords) 
+  proton(action='login', login=john$login, password=pass)
+
+# find Pietraszko in employees
+pietraszko <-  employees[which(employees$surname == 'Pietraszko'),]
+
+# pietraszko logins
+logs %>%
+filter( login == pietraszko$login) %>%
+count(host) %>%
+arrange(desc(n),) %>%
+  # convert using as.character
+  # factors are strange...
+lapply(as.character) %>%
+data.frame(stringsAsFactors = FALSE)-> host_counts
+
+# Next proton step
+proton(action = "server", host = as.character(host_counts[1,1]))
+
+# Find password
+hist <- data.frame(bsh=bash_history, stringsAsFactors = FALSE) %>%
+  transmute(first_proc = sapply(strsplit(bsh," "), function(i) i[1])) %>%
+  distinct()
+
+pass <- "DHbb7QXppuHnaXGN"
+proton(action = "login",login = pietraszko$login, password = pass)
